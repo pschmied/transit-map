1175dc4d5a655c9fad3503a0200b0539bf561b80
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-02-18 10:08:25 -0800

    initial project Skeletor

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject transit-map "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :plugins [[lein-localrepo "0.5.3"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [quil "2.2.5"]
+                 [unfolding "0.9.6"]])
